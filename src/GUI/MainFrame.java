9bb7d46e98677f195b74e35ec132a49e9646a59e
069949345 <069949345@109X0310JNVZ5V1.EDU.YRDSB.NET>
2019-01-15 13:37:43 -0500

    Merge origin/master
    Conflicts:
    	src/GUI/LoginScreen.java

--- src/GUI/MainFrame.java
+++ src/GUI/MainFrame.java
@@ -16,8 +16,6 @@ public class MainFrame extends javax.swing.JFrame {
      */
     public MainFrame() {
         initComponents();
-        cards.addLayoutComponent(loginScreen1, "Login Screen");
-        cards.addLayoutComponent(bookInfoScreen1, "Book Screen");
         cards.layoutContainer(this);
     }
 
@@ -30,9 +28,6 @@ public class MainFrame extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        loginScreen1 = new GUI.LoginScreen();
-        bookInfoScreen1 = new GUI.BookInfoScreen();
-
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setSize(new java.awt.Dimension(575, 510));
 
@@ -40,16 +35,11 @@ public class MainFrame extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(110, 110, 110)
-                .addComponent(bookInfoScreen1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(120, Short.MAX_VALUE))
+            .addGap(0, 805, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(0, 13, Short.MAX_VALUE)
-                .addComponent(bookInfoScreen1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGap(0, 523, Short.MAX_VALUE)
         );
 
         pack();
@@ -102,7 +92,5 @@ public class MainFrame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private GUI.BookInfoScreen bookInfoScreen1;
-    private GUI.LoginScreen loginScreen1;
     // End of variables declaration//GEN-END:variables
 }
